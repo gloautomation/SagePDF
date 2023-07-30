0b1d684018e9ff9891d1b393294b989091a245d8
Your Name <45288360+gloautomation@users.noreply.github.com>
2023-07-30 14:48:14 +0530

    first commit

---
+++ src/css.py
@@ -0,0 +1,15 @@
+v1 = """
+/* feedback checkbox */
+.css-18fuwiq {
+ position: relative;
+ padding-top: 6px;
+}
+.css-949r0i {
+ position: relative;
+ padding-top: 6px;
+ 
+.css-1544g2n {
+ padding-top: 10px;
+}
+
+"""
